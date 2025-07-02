15e08b470881b8aae29fecc3cc2ad36e383b5e81
Pavel Roskin <plroskin@gmail.com>
2025-07-01 20:37:35 -0700

    Use callPackage, make package.nix a separate file

---
+++ prmers/package.nix
@@ -0,0 +1,45 @@
+{
+  curl,
+  fetchFromGitHub,
+  lib,
+  ocl-icd,
+  opencl-headers,
+  stdenv,
+}:
+
+stdenv.mkDerivation (finalAttrs: {
+  pname = "prmers";
+  version = "4.0.22-alpha";
+
+  src = fetchFromGitHub {
+    owner = "cherubrock-seb";
+    repo = "prmers";
+    rev = "v${finalAttrs.version}";
+    sha256 = "sha256-Rnt2nlI+4GyuB7EoGUiYqhxvUhBkNZoO40hWcI4v3d8=";
+  };
+
+  buildInputs = [
+    curl
+    ocl-icd
+    opencl-headers
+  ];
+
+  PREFIX = placeholder "out";
+
+  enableParallelBuilding = true;
+
+  meta = {
+    description = "GPU-accelerated Mersenne Primality Test";
+    longDescription = ''
+      PrMers is a high-performance GPU application for testing the primality
+      of Mersenne numbers using the Lucas–Lehmer and PRP (Probable Prime)
+      algorithms. It leverages OpenCL and Number Theoretic Transforms (NTT)
+      for fast large-integer arithmetic, and is optimized for long-running
+      computations.
+    '';
+    homepage = "https://www.mersenneforum.org/node/1074417";
+    license = lib.licenses.mit;
+    platforms = lib.platforms.all;
+    mainProgram = "prmers";
+  };
+})
